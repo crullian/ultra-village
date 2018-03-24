4540b16754c38292d492ac4bf7be0fc7af68e173
crullian <crullian@gmail.com>
2018-03-24 11:44:02 -0400

    Improve syntax, create Artist Schema.

--- api/models/index.js
+++ api/models/index.js
@@ -1,14 +1,31 @@
-var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/wikistack', {useMongoClient: true});
-var db = mongoose.connection;
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/ultra-village');
+const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'mongodb connection error:'));
 
-var Page;
-var User;
+let Artist;
+let Page;
+let User;
 
-var Schema = mongoose.Schema;
+const Schema = mongoose.Schema;
 
-var pageSchema = new Schema({ // Schema describes the model 
+const artistSchema = new Schema({
+  artist_name: {
+    type:[String],
+    get: artist_name => {
+      return artist_name.join(", ");
+    }
+  },
+  pages: [{ 
+    type: Schema.Types.ObjectId,
+    ref: 'Page'
+  }]
+});
+
+const pageSchema = new Schema({ // Schema describes the model 
+  artist_name: [{ 
+    type: String
+  }],
   title: {
     type: String,
     required: true
@@ -19,58 +36,43 @@ var pageSchema = new Schema({ // Schema describes the model
     type: String,
     required: true
   },
-  // date: Date,
-  date: {
+  created_date: {
     type: Date,
     default: Date.now
   },
   status: Number,
   tags: {
     type: [String],
-    get: function(tags) {
+    get: tags => {
       return tags.join(", ");
     }
   }
-})
+});
 
-var userSchema = new Schema({
+const userSchema = new Schema({
   name: {
     first: String,
     last: String
   },
   email: String,
   isAdmin: Boolean
-})
-
-// pageSchema.methods.computeUrlName = function() {
-//   if (this.title.length > 0) { // undefined is taken care of in model, required: true
-//     this.url_name = this.title.replace(/[\W\s]/g, '_');
-//   }
-// }
-// pageSchema.pre('save', function(next) {
-//   this.computeUrlName();
-//   next();
-// })
+});
 
 pageSchema.virtual('full_route').get(function() { //what is .get
   return "/wiki/" + this.url_name; // why add wiki?
-})
+});
 
 // pageSchema.virtual('humanReadableDate').get(function() {
 //   console.log('VIRT', this.date)
 //   return this.date.toString(); 
 // })
 
-// pageSchema.virtual('full_route') { Why not this?
-//   return "/wiki/" + url_name;
-// })
-// how do you get the virtual? Page.virtual? 
-
-
+Artist = mongoose.model('Artist', artistSchema);
 Page = mongoose.model('Page', pageSchema);
 User = mongoose.model('User', userSchema);
 
 module.exports = {
+  "Artist": Artist,
   "Page": Page,
-  "User": User // how does this work?
+  "User": User
 };
