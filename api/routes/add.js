48b2f1e16676a4cf9a47118903506e16d771fc4e
crullian <crullian@gmail.com>
2018-03-24 11:46:43 -0400

    Improve syntax, comment on submit logic.

--- api/routes/add.js
+++ api/routes/add.js
@@ -1,43 +1,48 @@
-var express = require('express');
-var router = express.Router();
-var models = require('../models/');
+const express = require('express');
+const router = express.Router();
+const models = require('../models/');
 
-router.get('/', function(req, res, next) {
+const urlNameMaker = title => {
+  let url;
+  if (typeof title != "undefined" && title != "") {
+    // url = title.replace(/[\W\s]/g, '_');
+    url = title.replace(/\s/ig, '_').replace(/\W/ig,'');
+    //url = title.replace(/[\W]/g, '').replace(/[\s]/g, '_'); // replace whitespace w underscore
+    if (url.charAt(url.length - 1) === ('_')) {
+      url = url.substring(0, url.length - 1);
+    }
+  } else {
+    url = Math.random().toString(36).substring(2, 7);
+  }
+  return url;
+};
+
+router.get('/', (req, res, next) => {
   res.render('add', {
     title: 'Add an artist:'
   });
 });
 
-router.post('/submit', function(req, res, next) {
+router.post('/submit', (req, res, next) => {
+  const artist_name = req.body.artist_name;
+  const title = req.body.pageTitle; // req.params is url "constiable" :name, :id, etc
+  const body = req.body.pageContent;
+  const url_name = urlNameMaker(title);
 
-  var title = req.body.pageTitle; // req.params is url "variable" :name, :id, etc
-  var body = req.body.pageContent;
-
-  var urlNameMaker = function(title) {
-    var url;
-    if (typeof title != "undefined" && title != "") {
-      // url = title.replace(/[\W\s]/g, '_');
-      url = title.replace(/\s/ig, '_').replace(/\W/ig,'');
-      //url = title.replace(/[\W]/g, '').replace(/[\s]/g, '_'); // replace whitespace w underscore
-      if (url.charAt(url.length - 1) === ('_')) {
-        url = url.substring(0, url.length - 1);
-      }
-    } else {
-      url = Math.random().toString(36).substring(2, 7);
-    }
-    return url;
-  };
-
-  var url_name = urlNameMaker(title);
-
-  var p = new models.Page({
+  // Check if artist_name exists in artistModel
+  // if not create the artist
+  //   then create the page
+  // else
+  //   match artist_name with appropriate artistModel
+  //   create page 
+  //   push page id into artistModel's pages array
+  const page = new models.Page({
     "title": title,
     "body": body,
-    "url_name": url_name,
-
+    "url_name": url_name
   });
 
-  p.save();
+  page.save();
   res.redirect('/');
 });
 
