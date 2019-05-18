bfb4e0a14dd6028fac92a1c8fbc0a17538b71049
crullian <crullian@gmail.com>
2019-05-18 23:45:44 +0100

    Use a theme to inforce font.

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,7 @@
 import 'react-app-polyfill/ie9';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
 import './index.css';
 import App from './components/App/App';
 import ScrollToTop from './components/ScrollToTop/';
@@ -8,11 +9,19 @@ import {
   BrowserRouter as Router
 } from 'react-router-dom';
 
+const theme = createMuiTheme({
+  typography: {
+    fontFamily: 'Google Sans'
+  },
+});
+
 ReactDOM.render(
-  <Router>
-    <ScrollToTop>
-      <App />
-    </ScrollToTop>
-  </Router>,
+	<MuiThemeProvider theme={theme}>
+	  <Router>
+	    <ScrollToTop>
+	      <App />
+	    </ScrollToTop>
+	  </Router>
+  </MuiThemeProvider>,
   document.getElementById('root')
 );
