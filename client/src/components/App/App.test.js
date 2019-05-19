52a1aec594845b90c19ed10b310a90f7bc8bcffd
crullian <crullian@gmail.com>
2019-05-19 10:22:10 +0100

    Fix spaces.

--- client/src/components/App/App.test.js
+++ client/src/components/App/App.test.js
@@ -8,48 +8,48 @@ import {
 import { mount } from 'enzyme';
 
 const wrapper = mount(
-	<Router>
+  <Router>
     <App />
   </Router>
 );
 
 const mockItems = [
-	{
-		artist_name: 'artist',
-		albums: [],
-		body: 'content',
-		image: 'imageURL',
-	},
-	{
-		artist_name: 'artist',
-		albums: [],
-		body: 'content',
-		image: 'imageURL',
-	},
-	{
-		artist_name: 'artist',
-		albums: [],
-		body: 'content',
-		image: 'imageURL',
-	}
+  {
+    artist_name: 'artist',
+    albums: [],
+    body: 'content',
+    image: 'imageURL',
+  },
+  {
+    artist_name: 'artist',
+    albums: [],
+    body: 'content',
+    image: 'imageURL',
+  },
+  {
+    artist_name: 'artist',
+    albums: [],
+    body: 'content',
+    image: 'imageURL',
+  }
 ];
 
 describe('App root', () => {
-	it('should render ok', () => {
-	  expect(wrapper.find('App').exists()).toBe(true);
-	});
+  it('should render ok', () => {
+    expect(wrapper.find('App').exists()).toBe(true);
+  });
 
-	it('should show a loader initially', () => {
-		expect(wrapper.find('Loader').exists()).toBe(true);
-		expect(wrapper.find('ArtistList').exists()).toBe(false);
-	});
+  it('should show a loader initially', () => {
+    expect(wrapper.find('Loader').exists()).toBe(true);
+    expect(wrapper.find('ArtistList').exists()).toBe(false);
+  });
 
 
-	it('should render a list of entries when supplied data', () => {
+  it('should render a list of entries when supplied data', () => {
     const AppInstance = wrapper.find('App').instance();
     AppInstance.setState({items: mockItems, isLoading: false});
     wrapper.update();
-		expect(wrapper.find('ArtistList').exists()).toBe(true);
+    expect(wrapper.find('ArtistList').exists()).toBe(true);
     expect(wrapper.find('Loader').exists()).toBe(false);
-	});
+  });
 });
