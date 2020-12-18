a3de150c4f4ed69373a188af61da1698da1cb607
crullian <crullian@gmail.com>
2020-12-18 19:51:24 +0000

    WIP: adding analytics.

--- client/src/index.js
+++ client/src/index.js
@@ -8,6 +8,8 @@ import ScrollToTop from './components/ScrollToTop/';
 import {
   BrowserRouter as Router
 } from 'react-router-dom';
+import ReactGA from 'react-ga';
+import { createBrowserHistory } from 'history';
 
 const theme = createMuiTheme({
   typography: {
@@ -27,13 +29,30 @@ const theme = createMuiTheme({
   },
 });
 
+export default function Main() {
+
+  const history = createBrowserHistory();
+
+  // Initialize google analytics page view tracking
+  history.listen(location => {
+    console.log('Listening')
+    ReactGA.initialize(process.env.REACT_APP_GA_ID);
+    ReactGA.set({ page: location.pathname }); // Update the user's current page
+    ReactGA.pageview(location.pathname); // Record a pageview for the given page
+  });
+
+  return (
+    <MuiThemeProvider theme={theme}>
+      <Router history={history}>
+        <ScrollToTop>
+          <App />
+        </ScrollToTop>
+      </Router>
+    </MuiThemeProvider>
+  );
+}
+
 ReactDOM.render(
-  <MuiThemeProvider theme={theme}>
-    <Router>
-      <ScrollToTop>
-        <App />
-      </ScrollToTop>
-    </Router>
-  </MuiThemeProvider>,
+  <Main />,
   document.getElementById('root')
 );
