ebb4c28d4539262bf014126019f09676694dee6a
crullian <crullian@gmail.com>
2020-12-29 17:43:34 +0000

    Update to use firebase measurementId and comment out ReactGA.

--- client/src/index.js
+++ client/src/index.js
@@ -8,7 +8,7 @@ import ScrollToTop from './components/ScrollToTop/';
 import {
   BrowserRouter as Router
 } from 'react-router-dom';
-import ReactGA from 'react-ga';
+// import ReactGA from 'react-ga';
 import { createBrowserHistory } from 'history';
 
 const theme = createMuiTheme({
@@ -34,12 +34,12 @@ export default function Main() {
   const history = createBrowserHistory();
 
   // Initialize google analytics page view tracking
-  history.listen(location => {
-    console.log('Listening')
-    ReactGA.initialize(process.env.REACT_APP_GA_ID);
-    ReactGA.set({ page: location.pathname }); // Update the user's current page
-    ReactGA.pageview(location.pathname); // Record a pageview for the given page
-  });
+  // history.listen(location => {
+  //   console.log('Listening')
+  //   ReactGA.initialize(process.env.REACT_APP_GA_ID);
+  //   ReactGA.set({ page: location.pathname }); // Update the user's current page
+  //   ReactGA.pageview(location.pathname); // Record a pageview for the given page
+  // });
 
   return (
     <MuiThemeProvider theme={theme}>
